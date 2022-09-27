341288f6269b2e955392c87812e4dc5b573bdae1
fl-sll <edwardalvin@gmail.com>
2022-09-27 21:50:32 +0700

    updated image

--- Assignment1/assignment1.py
+++ Assignment1/assignment1.py
@@ -6,22 +6,22 @@ list = []
 
 session = HTMLSession()
 
+
 for sn in range(1,10):
         response = session.get('https://www.imdb.com/title/tt0460649/episodes?season=' + str(sn))
 
         soup = BeautifulSoup(response.text, 'html.parser')
 
         episode_containers = soup.find_all('div', class_ = 'info')
+        image_container = soup.find('div', class_ = 'image')
 
         for episodes in episode_containers:
                 season = sn
                 episode_number = episodes.meta['content']
                 title = episodes.a['title']
-                
-                #! images comes out as spinning-progress.gis
-                #// most probably because imdb blocks the scrapping of episode images
-                #// already tried for other series and results in the same spinning-progress.gif
-                images = episodes.img['src']
+
+                for image in image_container:
+                        images = image_container.img['src']
 
                 links = episodes.a['href']
 
@@ -40,7 +40,6 @@ for sn in range(1,10):
                 episodes_data = [season, episode_number, images, title, links, airdate, rating, total_votes, desc]
 
                 list.append(episodes_data)
-
 
 # response = session.get('https://www.imdb.com/title/tt0460649/episodes?season=2')
 # soup = BeautifulSoup(response.text, 'html.parser')
